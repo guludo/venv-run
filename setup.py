cfd738197fe4397787973e82491b7f0c04064af2
Gustavo Sousa <gustavo.jo.sousa@gmail.com>
2019-02-06 18:43:46 -0200

    Improve setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,26 @@
 from setuptools import setup, find_packages
 
+with open("README.rst", "r") as f:
+    long_description = f.read()
+
 setup(
     name='venv-run',
     version='0.0.0',
     author='Gustavo José de Sousa',
     author_email='gustavo.jo.sousa@gmail.com',
+    description='Run commands using Python virtual environment',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    url='https://github.com/guludo/venv-run',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'Topic :: Software Development',
+        'Topic :: Utilities',
+    ],
     package_dir={'': 'src'},
     py_modules=['venvrun'],
     python_requires='~=3.5',
