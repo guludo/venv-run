3fc293d95236b3e00f55718314d97055418d75be
Gustavo Sousa <gustavo.jo.sousa@gmail.com>
2019-02-06 18:35:11 -0200

    Add author information in setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='venv-run',
     version='0.0.0',
+    author='Gustavo José de Sousa',
+    author_email='gustavo.jo.sousa@gmail.com',
     package_dir={'': 'src'},
     py_modules=['venvrun'],
     python_requires='~=3.5',
