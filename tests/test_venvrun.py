7a3f3a1522ad21292abdda06d096adc5064bd1b6
Ville Skyttä <ville.skytta@iki.fi>
2022-02-26 19:16:27 +0200

    test: add symlink target deduplication test case

--- tests/test_venvrun.py
+++ tests/test_venvrun.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import tempfile
 import unittest
 from subprocess import run as real_subprocess_run
 from unittest.mock import patch
@@ -56,3 +57,26 @@ class VenvRunTest(unittest.TestCase):
                             'venv', '.venv', os.curdir
                     ))
             )
+
+    def testGuessDedupeSymlink(self):
+        with tempfile.TemporaryDirectory(prefix="venv-run") as tempdir:
+            symlink = os.path.join(tempdir, "venv")
+            os.symlink(os.path.join(os.getcwd(), "venv"), symlink)
+
+            def mock_run(*args, **kwargs):
+                if args[0] == ('pyenv', 'prefix', 'venv-run-testsuite'):
+                    return real_subprocess_run(
+                        ('echo', symlink), **kwargs)
+                return real_subprocess_run(*args, **kwargs)
+
+            with patch.object(venvrun.platform, "system", return_value='Linux'), \
+                 patch.object(venvrun.subprocess, "run", mock_run):
+                venvs = venvrun.guess()
+                self.assertListEqual(
+                    sorted(venvs),
+                    sorted(
+                        os.path.normpath(os.path.join(os.getcwd(), x))
+                        for x in (
+                                'venv', '.venv', os.curdir
+                        ))
+                )
