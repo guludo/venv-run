0339d1500db658067b4db7d6cdb6bb2ebd208a69
Gustavo Sousa <gustavo.jo.sousa@gmail.com>
2022-03-07 07:12:17 -0300

    chore: refactor code so that we pass mypy --strict

--- tests/test_venvrun.py
+++ tests/test_venvrun.py
@@ -1,8 +1,11 @@
 import os
+import platform
+import subprocess
 import sys
 import tempfile
 import unittest
 from subprocess import CompletedProcess, run as real_subprocess_run
+from typing import Any
 from unittest.mock import patch
 
 sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir, 'src'))
@@ -20,14 +23,14 @@ class VenvRunTest(unittest.TestCase):
 
     def testGuess(self) -> None:
 
-        def mock_run(*args, **kwargs) -> CompletedProcess[str]:
+        def mock_run(*args: Any, **kwargs: Any) -> CompletedProcess[str]:
             if args[0] == ('pyenv', 'prefix', 'venv-run-testsuite'):
                 return real_subprocess_run(
                     ('echo', 'pyenv/path/somewhere'), **kwargs)
             return real_subprocess_run(*args, **kwargs)
 
-        with patch.object(venvrun.platform, "system", return_value='Linux'), \
-             patch.object(venvrun.subprocess, "run", mock_run):
+        with patch.object(platform, "system", return_value='Linux'), \
+             patch.object(subprocess, "run", mock_run):
             venvs = venvrun.guess()
             self.assertTrue(venvs[0], "subscriptability")
             self.assertListEqual(
@@ -41,14 +44,14 @@ class VenvRunTest(unittest.TestCase):
 
     def testGuessDedupe(self) -> None:
 
-        def mock_run(*args, **kwargs) -> CompletedProcess[str]:
+        def mock_run(*args: Any, **kwargs: Any) -> CompletedProcess[str]:
             if args[0] == ('pyenv', 'prefix', 'venv-run-testsuite'):
                 return real_subprocess_run(
                     ('echo', 'venv'), **kwargs)
             return real_subprocess_run(*args, **kwargs)
 
-        with patch.object(venvrun.platform, "system", return_value='Linux'), \
-             patch.object(venvrun.subprocess, "run", mock_run):
+        with patch.object(platform, "system", return_value='Linux'), \
+             patch.object(subprocess, "run", mock_run):
             venvs = venvrun.guess()
             self.assertTrue(venvs[0], "subscriptability")
             self.assertListEqual(
@@ -65,14 +68,14 @@ class VenvRunTest(unittest.TestCase):
             symlink = os.path.join(tempdir, "venv")
             os.symlink(os.path.join(os.getcwd(), "venv"), symlink)
 
-            def mock_run(*args, **kwargs) -> CompletedProcess[str]:
+            def mock_run(*args: Any, **kwargs: Any) -> CompletedProcess[str]:
                 if args[0] == ('pyenv', 'prefix', 'venv-run-testsuite'):
                     return real_subprocess_run(
                         ('echo', symlink), **kwargs)
                 return real_subprocess_run(*args, **kwargs)
 
-            with patch.object(venvrun.platform, "system", return_value='Linux'), \
-                 patch.object(venvrun.subprocess, "run", mock_run):
+            with patch.object(platform, "system", return_value='Linux'), \
+                 patch.object(subprocess, "run", mock_run):
                 venvs = venvrun.guess()
                 self.assertTrue(venvs[0], "subscriptability")
                 self.assertListEqual(
