d8412f297f5ebd907b5d77d3b4d1338756a1e53d
Ville Skyttä <ville.skytta@iki.fi>
2023-03-21 22:01:12 +0200

    style: reformat with black

--- setup.py
+++ setup.py
@@ -4,41 +4,41 @@ with open("README.rst", "r") as f:
     long_description = f.read()
 
 setup(
-    name='venv-run',
-    version='0.2.0',
-    author='Gustavo José de Sousa',
-    author_email='gustavo.jo.sousa@gmail.com',
-    description='Run commands using Python virtual environment',
+    name="venv-run",
+    version="0.2.0",
+    author="Gustavo José de Sousa",
+    author_email="gustavo.jo.sousa@gmail.com",
+    description="Run commands using Python virtual environment",
     long_description=long_description,
-    long_description_content_type='text/x-rst',
-    url='https://github.com/guludo/venv-run',
+    long_description_content_type="text/x-rst",
+    url="https://github.com/guludo/venv-run",
     project_urls={
-        'Changelog': 'https://github.com/guludo/venv-run/blob/master/CHANGELOG.md',
+        "Changelog": "https://github.com/guludo/venv-run/blob/master/CHANGELOG.md",
     },
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
-        'Topic :: Software Development',
-        'Topic :: Utilities',
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Topic :: Software Development",
+        "Topic :: Utilities",
     ],
-    package_dir={'': 'src'},
-    py_modules=['venvrun'],
-    python_requires='~=3.7',
+    package_dir={"": "src"},
+    py_modules=["venvrun"],
+    python_requires="~=3.7",
     entry_points={
-        'console_scripts': [
-            'venv-run = venvrun:run',
+        "console_scripts": [
+            "venv-run = venvrun:run",
         ]
     },
     extras_require={
-        'dev': [
-            'black==23.1.0',
-            'mypy==1.1.1',
-            'nox',
-            'types-setuptools',
-            'ruff==0.0.257',
+        "dev": [
+            "black==23.1.0",
+            "mypy==1.1.1",
+            "nox",
+            "types-setuptools",
+            "ruff==0.0.257",
         ],
     },
 )
--- src/venvrun.py
+++ src/venvrun.py
@@ -9,47 +9,49 @@ from typing import List
 
 
 def guess() -> List[str]:
-    if platform.system() != 'Windows':
-        pypath = ('bin', 'python')
+    if platform.system() != "Windows":
+        pypath = ("bin", "python")
     else:
-        pypath = ('Scripts', 'python.exe')
+        pypath = ("Scripts", "python.exe")
 
     exes = []
 
-    exes.extend(glob(os.path.join('*', *pypath)))
-    exes.extend(glob(os.path.join('.*', *pypath)))
+    exes.extend(glob(os.path.join("*", *pypath)))
+    exes.extend(glob(os.path.join(".*", *pypath)))
     exes.append(os.path.join(os.path.curdir, *pypath))
-    exes.extend(glob(os.path.join('.direnv', 'python-*', *pypath)))
-    exes.extend(glob(os.path.join('.direnv', 'virtualenv', *pypath)))
+    exes.extend(glob(os.path.join(".direnv", "python-*", *pypath)))
+    exes.extend(glob(os.path.join(".direnv", "virtualenv", *pypath)))
     try:
-        with open('.python-version', 'r') as f:
-            for prefix in (x.strip() for x in f if not re.match(r'\s*#', x)):
+        with open(".python-version", "r") as f:
+            for prefix in (x.strip() for x in f if not re.match(r"\s*#", x)):
                 proc = subprocess.run(
-                    ('pyenv', 'prefix', prefix),
-                    stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+                    ("pyenv", "prefix", prefix),
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.DEVNULL,
+                )
                 exes.append(os.path.join(proc.stdout.decode().strip(), *pypath))
                 break
     except FileNotFoundError:
         pass
     try:
-        with open('.tool-versions', 'r') as f:
+        with open(".tool-versions", "r") as f:
             for line in f:
-                tool_version = line.partition('#')[0].strip().split()
+                tool_version = line.partition("#")[0].strip().split()
                 if len(tool_version) == 2 and tool_version[0] == "python":
                     proc = subprocess.run(
-                        ('asdf', 'where', *tool_version),
-                        stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+                        ("asdf", "where", *tool_version),
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.DEVNULL,
+                    )
                     if proc.returncode == 0:  # may spew errors to stdout
-                        exes.append(
-                            os.path.join(proc.stdout.decode().strip(), *pypath))
+                        exes.append(os.path.join(proc.stdout.decode().strip(), *pypath))
                     break
     except FileNotFoundError:
         pass
 
     venvs = dict(
         # Absolutize, normalize, and canonicalize for deduplication
-        (os.path.realpath(os.path.abspath(
-            os.path.dirname(os.path.dirname(exe)))), True)
+        (os.path.realpath(os.path.abspath(os.path.dirname(os.path.dirname(exe)))), True)
         for exe in exes
         if os.access(exe, os.X_OK)
     )
@@ -59,8 +61,8 @@ def guess() -> List[str]:
 
 def run() -> None:
     parser = ArgumentParser(
-        usage='%(prog)s [OPTIONS] [--] [CMD]',
-        description='''
+        usage="%(prog)s [OPTIONS] [--] [CMD]",
+        description="""
             Run command from an existing python virtual environment (that is,
             with the environment's bin directory prepended to PATH). By default
             the location of the virtual environment directory is searched from
@@ -79,55 +81,57 @@ def run() -> None:
             ends with '.py', then 'python' is prepended to CMD and the
             execution is retried. If you do not desire such a behavior, pass
             the --no-guess option.
-        ''',
+        """,
     )
 
-    parser.add_argument('--venv',
-        help='''
+    parser.add_argument(
+        "--venv",
+        help="""
             Use this virtual environment instead of searching for one.
-        ''',
+        """,
     )
 
-    parser.add_argument('--no-guess',
-        action='store_true',
-        help='''
+    parser.add_argument(
+        "--no-guess",
+        action="store_true",
+        help="""
             Do not try to prepend 'python' when execution fails because the
             command is not found.
-        ''',
+        """,
     )
 
     args, cmd_args = parser.parse_known_args()
 
     if not cmd_args:
-        cmd_args = ['python']
+        cmd_args = ["python"]
 
-    if cmd_args and cmd_args[0] == '--':
+    if cmd_args and cmd_args[0] == "--":
         cmd_args.pop(0)
     if not args.venv:
         venvs = guess()
 
         if not venvs:
-            print('No virtual environments found', file=sys.stderr)
+            print("No virtual environments found", file=sys.stderr)
             exit(1)
 
         if len(venvs) > 1:
-            print('More than one virtual environment found:', file=sys.stderr)
+            print("More than one virtual environment found:", file=sys.stderr)
             for p in venvs:
-                print('  ' + p, file=sys.stderr)
-            print('Please, use the --venv option.', file=sys.stderr)
+                print("  " + p, file=sys.stderr)
+            print("Please, use the --venv option.", file=sys.stderr)
             exit(1)
 
         args.venv = venvs[0]
-    path = ''
-    if platform.system() != 'Windows':
-        path = os.path.join(args.venv, 'bin')
+    path = ""
+    if platform.system() != "Windows":
+        path = os.path.join(args.venv, "bin")
     else:
-        path = os.path.join(args.venv, 'Scripts')
+        path = os.path.join(args.venv, "Scripts")
 
-    if 'PATH' in os.environ:
-        os.environ['PATH'] = path + os.pathsep + os.environ['PATH']
+    if "PATH" in os.environ:
+        os.environ["PATH"] = path + os.pathsep + os.environ["PATH"]
     else:
-        os.environ['PATH'] = path
+        os.environ["PATH"] = path
 
     try:
         os.execvp(cmd_args[0], cmd_args)
@@ -135,12 +139,12 @@ def run() -> None:
         if args.no_guess:
             raise
 
-        if cmd_args[0].startswith('-') or cmd_args[0].endswith('.py'):
-            cmd_args.insert(0, 'python')
+        if cmd_args[0].startswith("-") or cmd_args[0].endswith(".py"):
+            cmd_args.insert(0, "python")
             os.execvp(cmd_args[0], cmd_args)
         else:
             raise
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run()
